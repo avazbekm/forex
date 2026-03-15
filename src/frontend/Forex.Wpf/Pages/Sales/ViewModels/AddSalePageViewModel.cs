48fba19e03c710520fa49baf7a1f06bd29f0ee47
muqimjon <muqimjon.mr@gmail.com>
2026-03-15 19:26:19 +0500

    fix: Restore missing using statements in AddSalePageViewModel

--- src/frontend/Forex.Wpf/Pages/Sales/ViewModels/AddSalePageViewModel.cs
+++ src/frontend/Forex.Wpf/Pages/Sales/ViewModels/AddSalePageViewModel.cs
@@ -15,8 +15,15 @@ using MapsterMapper;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
-
-namespace Forex.Wpf.Pages.Sales.ViewModels;
+using System.IO;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 public partial class AddSalePageViewModel : ViewModelBase
 {
